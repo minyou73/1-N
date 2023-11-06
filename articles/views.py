e37a30bc90aedbcf78ea34600373a767638cc923
minyou73 <145949811+minyou73@users.noreply.github.com>
2023-11-06 10:56:10 +0900

    create기능구현

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
 def index(request):
@@ -10,6 +11,7 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
+
 def detail(request, id):
     article = Article.objects.get(id=id)
 
@@ -17,3 +19,18 @@ def detail(request, id):
         'article': article
     }
     return render(request, 'detail.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', id=article.id)
+    else:
+        form = ArticleForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'form.html', context)
